9ae5553872e6b5bbd414480db882db9312271a00
haruki,hirai <hirai-haruki@jrem.co.jp>
2023-11-20 18:20:35 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/haruki/taskapp/SecondActivity.kt
@@ -0,0 +1,56 @@
+package jp.techacademy.haruki.taskapp
+
+import android.app.AlarmManager
+import android.app.AlarmManager.AlarmClockInfo
+import android.app.DatePickerDialog
+import android.app.PendingIntent
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import jp.techacademy.haruki.taskapp.databinding.ActivityInputBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Locale.Category
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInputBinding
+    private lateinit var realm: Realm
+    private lateinit var task: Task
+    private lateinit var category: Category
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // アクションバーの設定
+        setSupportActionBar(binding.toolbar)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // EXTRA_TASKからTaskのidを取得
+        val intent = intent
+        val taskId = intent.getIntExtra(EXTRA_TASK, -1)
+
+        // Realmデータベースとの接続を開く
+        val config = RealmConfiguration.create(schema = setOf(Task::class))
+        realm = Realm.open(config)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Realmデータベースとの接続を閉じる
+        realm.close()
+    }
+
+
+}
